032c463cb480190e264e13f5b5a3e3936620817d
Padpach <emmanuel.pacheco@api4web.com>
2015-04-07 16:35:45 -0500

    agregamos un registro desde el db_create

--- app/db_create.py
+++ app/db_create.py
@@ -10,10 +10,10 @@ from datetime import date
 
 
 # Cremos la Base de Datos
-db.create_all()
+#db.create_all()
 
 #insertamos
-#db.session.add(Task("Finish this Tutorial", date(1015, 04, 01), 10, 1))
+db.session.add(User("admin", "emma.pacheco@gmail.com", "adminadmin", "admin"))
 #db.session.add(Task("Terminar el Tuto 3", date(2014, 03, 13), 6, 1))
 #db.session.add(Task("Love Life", date(2015, 05, 05), 6, 1))
 
